3f3e2a30eceedb32bdffcc9d0f0a0ad05c163543
triplelog <robmwilc@gmail.com>
2020-04-11 14:48:50 -0400

    First

--- server/gifsaw-server.js
+++ server/gifsaw-server.js
@@ -142,8 +142,6 @@ function makelines(width,height,npieces) {
 	width = 512/(276*2+512); //Revert to 1
 	height = 144/164; //Revert to 1
 	
-	width = 512;
-	height = 288;
 	
 	
 	const xchoices = [-1*width/ncols*.25,-1*width/ncols*.15,width/ncols*.15,width/ncols*.25];
