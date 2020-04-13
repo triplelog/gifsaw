01e1a3f06955bb6c22d0caf9fb8c04bd999c300d
triplelog <robmwilc@gmail.com>
2020-04-13 17:53:32 -0400

    First

--- server/models/puzzle.js
+++ server/models/puzzle.js
@@ -2,5 +2,5 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const Puzzle = new Schema({id:'',matches:[],initialScript:''});
+const Puzzle = new Schema({id:'',matches:{},initialScript:''});
 module.exports = mongoose.model('Puzzle', Puzzle);
