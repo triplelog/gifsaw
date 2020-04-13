a0603928ad230048755859067d8cf45ca7aa1ce0
triplelog <robmwilc@gmail.com>
2020-04-12 23:50:08 -0400

    First

--- server/gifsaw-server.js
+++ server/gifsaw-server.js
@@ -316,7 +316,7 @@ wss.on('connection', function connection(ws) {
 					}
 					if (tomatch.length>0 && myroom){
 						var acceptMatch = updateScores(myroom,[tomatch,username]);
-						console.log(vm.run('players'));
+						console.log(myroom.vm.run('players'));
 						if (acceptMatch !== false){
 							var jsonmessage = {'type':'foundMatch','message':['video'+dm.message[0],tomatch,'me']}
 							for (var i in myroom.players){
