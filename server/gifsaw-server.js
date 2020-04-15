043fa0081e50dedbbe71ed3aebcf39cc2458d380
triplelog <robmwilc@gmail.com>
2020-04-15 15:37:21 -0400

    First

--- server/gifsaw-server.js
+++ server/gifsaw-server.js
@@ -437,7 +437,7 @@ function makelines(vm,encryptedpuzzle,actwidth,actheight,nrows,ncols) {
 	var locations = [];
 	var rotations = [];
 	var matches = {};
-	var pointyFactor = .1;
+	var pointyFactor = .25;
 	const bottomcode = new VMScript(`
 	w = x1-x0;
 	if (w<0){w *= -1;}
